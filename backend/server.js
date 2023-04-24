4d50be7ba5e298b3b2d39db2579aaf3ba789609c
cwegner87 <cwegner1@live.maryville.edu>
2023-04-23 20:34:12 -0400

    backend server setup

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const app = require('./app')
+
+
+const dotenv = require('dotenv');
+
+//Setting up config file
+dotenv.config({ path: 'backend/config/config.env' })
+
+app.listen(process.env.PORT, () => {
+    console.log('Server started on Port: ${process.env.PORT} in ${process.env.NODE_ENV} mode')
+});
+
+
+
+
